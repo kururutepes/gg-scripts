4961868743bc76ff6057438c79819c6359fd1aaf
kururutepes <olivertodayonemoretime@gmail.com>
2026-05-11 14:42:56 +0700

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,20 +1,19 @@
--- menu.lua - รายการฟังก์ชันทั้งหมด
 return {
     {
         name = "🌀 วนบอส",
         type = "sub",
         menu = {
-            { name = "🐉 วาปบอส 1", file = "warp.lua", action = "boss1" },
-            { name = "🐉 วาปบอส 2", file = "warp.lua", action = "boss2" },
-            { name = "🐉 วาปบอส 3", file = "warp.lua", action = "boss3" }
+            { name = "🐉 วาปบอส 1", file = "Wrap.lua", action = "boss1" },
+            { name = "🐉 วาปบอส 2", file = "Wrap.lua", action = "boss2" },
+            { name = "🐉 วาปบอส 3", file = "Wrap.lua", action = "boss3" }
         }
     },
     {
         name = "🗺️ วาปนาวา",
         type = "sub",
         menu = {
-            { name = "🐉 นาวาบอส", file = "warp.lua", action = "boss" },
-            { name = "🗺️ นาวาแมพ", file = "warp.lua", action = "map" }
+            { name = "🐉 นาวาบอส", file = "Wrap.lua", action = "boss" },
+            { name = "🗺️ นาวาแมพ", file = "Wrap.lua", action = "map" }
         }
     },
     {
