b8649521d219f67f96d977ebf2a36fe966648ea8
kururutepes <olivertodayonemoretime@gmail.com>
2026-05-11 11:37:32 +0700

    Update and rename README.md to menu.lua

---
+++ menu.lua
@@ -0,0 +1,15 @@
+# gg-scripts-- menu.lua - รายการฟังก์ชันทั้งหมด
+return {
+    {
+        name = "วาปบอส",
+        icon = "🐉",
+        file = "warp.lua",
+        action = "boss"
+    },
+    {
+        name = "วาปแมพ",
+        icon = "🗺️",
+        file = "warp.lua",
+        action = "map"
+    }
+}
